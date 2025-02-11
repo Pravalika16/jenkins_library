09556e7d8ef66e632f847bda0fe0c1a436cb5af5
Pravalika16 <100553084+Pravalika16@users.noreply.github.com>
2025-02-11 16:13:52 +0530

    Update ansible_config.groovy

--- resources/ansible_config.groovy
+++ resources/ansible_config.groovy
@@ -1,1 +1,7 @@
-
+return [
+    SLACK_CHANNEL_NAME  : 'team-b-batch-29',
+    ENVIRONMENT         : 'prod',
+    CODE_BASE_PATH      : '/home/ansible',
+    ACTION_MESSAGE      : 'Deployment completed for production environment!',
+    KEEP_APPROVAL_STAGE : true
+]
